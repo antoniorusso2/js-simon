c139304623e60ff612934280e67cabb7b5375ac2
antoniorusso2 <russoantonio303@outlook.it>
2024-10-24 15:10:08 +0200

    ADD: files - index.html; style.css; script.js; amd README with project info

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//generatore numeri casuali da 1 a 50;
+// timer 30 secondi e poi i numeri scompaiono;
+//appaiono 5 input in cui l'utente inserisce i numero che ha visto precedentemente
+//il software risponde indicando quanti e quali numeri sono stati individuati correttamente
+//* non e' importante l'ordine dei numeri basta che ne inserisca il piu possibile corretti
+
